4bb423040c66702ff80a6626e6330b431332f7fb
larry.zhang <larry.zhang@yeahmobi.com>
2023-04-13 17:05:12 +0800

    first commit

---
+++ src/depthmap.js
@@ -0,0 +1,55 @@
+const PIXI = require('pixi.js');
+
+// import { Sprite } from 'pixi.js';
+
+module.exports.generate3dPhoto = function (options) {
+  let width;
+  let height;
+  const {
+    el, src, map, scale = 1
+  } = options;
+  const $target = document.querySelector(el);
+
+  console.log('$target', $target);
+
+  const image = new Image()
+  image.src = src
+  image.onload = function () {
+    width = image.width * scale;
+    height = image.height * scale;
+    $target.style.width = `${width}px`;
+    $target.style.height = `${height}px`;
+
+    // Generate PIXI app.
+    let app = new PIXI.Application({
+      width,
+      height
+    });
+    app.stage.width = width;
+    app.stage.height = height;
+    $target.appendChild(app.view);
+
+    // Generate Sprite object for original image.
+    let img = PIXI.Sprite.from(src);
+    img.width = width;
+    img.height = height;
+
+    // Generate Sprite object for depth map.
+    let depthMap = PIXI.Sprite.from(map);
+    depthMap.width = width;
+    depthMap.height = height;
+
+    // Generate displacement filter by depth map.
+    let displacementFilter = new PIXI.filters.DisplacementFilter(depthMap);
+
+    // Put on the stage.
+    app.stage.addChild(img);
+    app.stage.addChild(depthMap);
+    app.stage.filters = [displacementFilter];
+
+    $target.onmousemove = function(e) {
+      displacementFilter.scale.x = (width / 2 - e.clientX) / 20;
+      displacementFilter.scale.y = (height / 2 - e.clientY) / 20;
+    };
+  }
+}
---
+++ src/views/AboutView.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    <div id="pikachu"></div>
+  </div>
+</template>
+
+
+<script>
+// @ is an alias to /src
+import depth from '@/depthmap.js'
+
+export default {
+  name: 'HomeView',
+  // components: {
+  //   depth
+  // },
+  data() {
+    return {};
+  },
+  created() {
+  },
+  mounted() {
+    this.depthmapTest();
+  },
+  methods: {
+    depthmapTest() {
+      depth.generate3dPhoto({
+        el: '#pikachu', // Required!
+        src: 'pikachu.jpg', // Required!
+        map: 'pikachu.map.jpg', // Required!
+        scale: 0.7 // Optional, default: 1 
+      });
+    }
+  }
+}
+</script>
